9d6bb0ee3a9a915c657ac0677618baab765acb08
NicoFuentese <nicolas.a.fuentes.e@gmail.com>
2026-08-29 16:04:21 -0400

    inicio de app renderizada

--- check-chrome.js
+++
@@ -1,6 +0,0 @@
-const { app } = require('electron');
-app.whenReady().then(() => {
-  console.log('Chromium:', process.versions.chrome);
-  console.log('Electron:', process.versions.electron);
-  app.quit();
-});
